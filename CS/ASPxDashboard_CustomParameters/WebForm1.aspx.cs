f2cb70399ec4d8c76e26206589ec09c6ba348663
DevExpressExampleBot <DevExpressExampleBot@users.noreply.github.com>
2018-05-20 02:47:26 +0300

    initial commit

---
+++ CS/ASPxDashboard_CustomParameters/WebForm1.aspx.cs
@@ -0,0 +1,37 @@
+﻿using DevExpress.DashboardCommon;
+using DevExpress.DashboardWeb;
+using System;
+using System.Linq;
+
+namespace ASPxDashboard_CustomParameters
+{
+    public partial class WebForm1 : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            ASPxDashboard1.DashboardXmlPath = Server.MapPath("App_Data/dashboard1.xml");
+        }
+        protected void ASPxDashboard1_SetInitialDashboardState(object sender, SetInitialDashboardStateEventArgs e)
+        {
+            e.InitialState = InitializeDashboardState();
+        }
+
+        public DashboardState InitializeDashboardState()
+        {
+            DashboardState dashboardState = new DashboardState();
+            DashboardParameterState parameterState =
+                new DashboardParameterState("CustomerIdDashboardParameter", "XXX", typeof(string));
+            dashboardState.Parameters.Add(parameterState);
+            return dashboardState;
+        }
+
+        protected void ASPxDashboard1_CustomParameters(object sender, CustomParametersWebEventArgs e)
+        {
+            var custIDParameter = e.Parameters.FirstOrDefault(p => p.Name == "CustomerIdDashboardParameter");
+            if (custIDParameter != null)
+            {
+                custIDParameter.Value = "AROUT";
+            }
+        }
+    }
+}
---
+++ VB/ASPxDashboard_CustomParameters/WebForm1.aspx
@@ -0,0 +1,25 @@
+﻿<%@ Page Language="vb" AutoEventWireup="true" CodeBehind="WebForm1.aspx.vb" 
+    Inherits="ASPxDashboard_CustomParameters.WebForm1" %>
+
+<%@ Register Assembly="DevExpress.Dashboard.v17.2.Web, Version=17.2.7.0, Culture=neutral, PublicKeyToken=b88d1754d700e49a" 
+    Namespace="DevExpress.DashboardWeb" TagPrefix="dx" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+    <div style="position:absolute; left:0; right:0; top:0; bottom:0;">
+        <dx:ASPxDashboard ID="ASPxDashboard1" runat="server" 
+            WorkingMode="Viewer"
+            OnSetInitialDashboardState="ASPxDashboard1_SetInitialDashboardState"
+            OnCustomParameters="ASPxDashboard1_CustomParameters" 
+            Height="100%" Width="100%">
+        </dx:ASPxDashboard>
+    </div>
+    </form>
+</body>
+</html>
