09ddab89190079f80ce4ecb0bddaddcf17e2a62b
DevExpressExampleBot <fakeEmail>
2022-09-27 10:20:22 +0400

    Source auto update [skip ci]

--- VB/ASPxDashboard_CustomParameters/WebForm1.aspx.vb
+++ VB/ASPxDashboard_CustomParameters/WebForm1.aspx.vb
@@ -1,28 +1,30 @@
-﻿Imports DevExpress.DashboardCommon
+Imports DevExpress.DashboardCommon
 Imports DevExpress.DashboardWeb
 Imports System
 Imports System.Linq
 
 Namespace ASPxDashboard_CustomParameters
-    Partial Public Class WebForm1
-        Inherits System.Web.UI.Page
+
+    Public Partial Class WebForm1
+        Inherits Web.UI.Page
 
         Protected Sub Page_Load(ByVal sender As Object, ByVal e As EventArgs)
             ASPxDashboard1.DashboardXmlPath = Server.MapPath("App_Data/dashboard1.xml")
         End Sub
+
         Protected Sub ASPxDashboard1_SetInitialDashboardState(ByVal sender As Object, ByVal e As SetInitialDashboardStateEventArgs)
             e.InitialState = InitializeDashboardState()
         End Sub
 
         Public Function InitializeDashboardState() As DashboardState
-            Dim dashboardState As New DashboardState()
-            Dim parameterState As New DashboardParameterState("CustomerIdDashboardParameter", "XXX", GetType(String))
+            Dim dashboardState As DashboardState = New DashboardState()
+            Dim parameterState As DashboardParameterState = New DashboardParameterState("CustomerIdDashboardParameter", "XXX", GetType(String))
             dashboardState.Parameters.Add(parameterState)
             Return dashboardState
         End Function
 
         Protected Sub ASPxDashboard1_CustomParameters(ByVal sender As Object, ByVal e As CustomParametersWebEventArgs)
-            Dim custIDParameter = e.Parameters.FirstOrDefault(Function(p) p.Name = "CustomerIdDashboardParameter")
+            Dim custIDParameter = e.Parameters.FirstOrDefault(Function(p) Equals(p.Name, "CustomerIdDashboardParameter"))
             If custIDParameter IsNot Nothing Then
                 custIDParameter.Value = "AROUT"
             End If
